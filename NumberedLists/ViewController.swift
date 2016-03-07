b2edd8b3b96eb9088075ad65b988b3ad512cdf9f
Rhett Rogers <serenade.xs@icloud.com>
2016-03-07 10:43:38 -0700

    Adds trigger button and helper functions

--- NumberedLists/ViewController.swift
+++ NumberedLists/ViewController.swift
@@ -11,17 +11,30 @@ import UIKit
 class ViewController: UIViewController {
 
     var textView = UITextView()
-    
+    var triggerButton = UIButton(type: .InfoLight)
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.addSubview(textView)
+        view.addSubview(triggerButton)
+        
         textView.translatesAutoresizingMaskIntoConstraints = false
+        triggerButton.translatesAutoresizingMaskIntoConstraints = false
+        
         view.addConstraint(NSLayoutConstraint(item: view, attribute: .Width, relatedBy: .Equal, toItem: textView, attribute: .Width, multiplier: 1, constant: 0))
         view.addConstraint(NSLayoutConstraint(item: view, attribute: .CenterX, relatedBy: .Equal, toItem: textView, attribute: .CenterX, multiplier: 1, constant: 0))
         view.addConstraint(NSLayoutConstraint(item: view, attribute: .CenterY, relatedBy: .Equal, toItem: textView, attribute: .CenterY, multiplier: 1, constant: 0))
         textView.addConstraint(NSLayoutConstraint(item: textView, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 400))
         
+        view.addConstraint(NSLayoutConstraint(item: view, attribute: .CenterX, relatedBy: .Equal, toItem: triggerButton, attribute: .CenterX, multiplier: 1, constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: view, attribute: .Top, relatedBy: .Equal, toItem: triggerButton, attribute: .Top, multiplier: 1, constant: -100))
+        triggerButton.addConstraint(NSLayoutConstraint(item: triggerButton, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 22))
+        triggerButton.addConstraint(NSLayoutConstraint(item: triggerButton, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 22))
+
+        triggerButton.addTarget(self, action: "toggleNumberedList", forControlEvents: .TouchUpInside)
+
         textView.delegate = self
         
         view.backgroundColor = UIColor.blueColor()
@@ -33,7 +46,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func toggleNumberedList() {
 
+    }
+
+    func selectionContainsNumberedList(selection: NSRange) -> Bool {
+        return false
+    }
+
+    func previousNumberOfNumberedList(selection: NSRange) -> Int {
+        return -1
+    }
 }
 
 extension ViewController: UITextViewDelegate {
