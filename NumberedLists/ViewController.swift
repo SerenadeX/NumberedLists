59a3136f5b5105c50647b27b924b844f8eaff789
Rhett Rogers <serenade.xs@icloud.com>
2016-03-07 10:23:40 -0700

    Adds text view to view

--- NumberedLists/ViewController.swift
+++ NumberedLists/ViewController.swift
@@ -10,8 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var textView = UITextView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(textView)
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        view.addConstraint(NSLayoutConstraint(item: view, attribute: .Width, relatedBy: .Equal, toItem: textView, attribute: .Width, multiplier: 1, constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: view, attribute: .CenterX, relatedBy: .Equal, toItem: textView, attribute: .CenterX, multiplier: 1, constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: view, attribute: .CenterY, relatedBy: .Equal, toItem: textView, attribute: .CenterY, multiplier: 1, constant: 0))
+        textView.addConstraint(NSLayoutConstraint(item: textView, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: 400))
+        
+        textView.delegate = self
         
         view.backgroundColor = UIColor.blueColor()
         // Do any additional setup after loading the view, typically from a nib.
@@ -25,3 +36,10 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITextViewDelegate {
+    
+    func textViewDidChangeSelection(textView: UITextView) {
+        print(textView.selectedRange)
+    }
+    
+}
